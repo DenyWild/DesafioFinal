fb303e086e9820912bb74c7bc2674567a369dafe
DenyWild <denyswildson2011@gmail.com>
2023-10-22 22:14:55 -0300

    retirando metodos não utilizados

--- src/main/java/com/desafioFinal/DesafioFinal/DesafioFinalApplication.java
+++ src/main/java/com/desafioFinal/DesafioFinal/DesafioFinalApplication.java
@@ -4,12 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DesafioFinalApplication {
-
-//    public String PORT = System.getenv("PORT");
 
     @Bean
     public ModelMapper mapper() {
